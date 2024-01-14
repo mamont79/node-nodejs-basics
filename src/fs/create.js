d533749b4c641cdb25a4c691dd6e45a20acc63a3
Pavel Dalhou <dalhoupavel@gmail.com>
2024-01-14 15:38:38 +0100

    feat: add solution to create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
 const create = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, "fresh.txt");
+
+  try {
+    await fs.promises.access(filePath);
+    console.error("FS operation failed");
+  } catch (error) {
+    fs.writeFile(
+      filePath,
+      "I am fresh and young",
+      { overwrite: false },
+      function (err) {
+        if (err) throw err;
+        console.log("fresh.txt was created");
+      }
+    );
+  }
 };
 
 await create();
