3992e453455a605532fbc8e156ea3d4281b81b83
Pavel Dalhou <dalhoupavel@gmail.com>
2024-01-23 22:07:18 +0100

    feat: add solution to hash calculator

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { createHash } from "crypto";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const fileForHashCalc = path.join(
+  __dirname,
+  "files",
+  "fileToCalculateHashFor.txt"
+);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hashOfFile = createHash("sha256");
+  const fileHashStream = fs.createReadStream(fileForHashCalc);
+
+  fileHashStream.on("data", (data) => {
+    hashOfFile.update(data);
+  });
+
+  fileHashStream.on("end", () => {
+    const calculatedHash = hashOfFile.digest("hex");
+    console.log(`hash of file is:   ${calculatedHash}`);
+  });
 };
 
 await calculateHash();
