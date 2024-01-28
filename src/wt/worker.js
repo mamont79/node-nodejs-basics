adaa0e189f64b85e0595ae7dd23af61c7bf6a5bb
Pavel Dalhou <dalhoupavel@gmail.com>
2024-01-28 18:53:56 +0100

    feat: add solution worker

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,24 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort } from "worker_threads";
+
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  parentPort.on("message", (num) => {
+    let resultMessage = {
+      status: "error",
+      data: null,
+    };
+
+    if (typeof num === "number" && num > 0) {
+      resultMessage = {
+        status: "resolved",
+        data: nthFibonacci(num),
+      };
+    }
+    parentPort.postMessage(resultMessage);
+    parentPort.close();
+  });
 };
 
 sendResult();
